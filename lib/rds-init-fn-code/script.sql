b8bf8856606b013e49e9c33d655169019f447e0a
ajasharma73 <122118561+ajasharma73@users.noreply.github.com>
2023-01-10 22:20:41 +0000

    added mock data

--- lib/rds-init-fn-code/script.sql
+++ lib/rds-init-fn-code/script.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE CRUK;
+CREATE DATABASE IF NOT EXISTS CRUK;
 
 CREATE TABLE IF NOT EXISTS CRUK.users (
 	id INT auto_increment NOT NULL,
@@ -12,7 +12,7 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS CRUK.donation (
+CREATE TABLE IF NOT EXISTS CRUK.donations (
 	id INT auto_increment NOT NULL,
 	donation_amount BIGINT UNSIGNED DEFAULT 0 NOT NULL,
 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -28,8 +28,28 @@ CREATE TABLE IF NOT EXISTS CRUK.user_donation_map (
 	user_id INT NOT NULL,
 	donation_id INT NOT NULL,
 	CONSTRAINT user_donation_map_FK FOREIGN KEY (user_id) REFERENCES CRUK.users(id),
-	CONSTRAINT user_donation_map_FK_1 FOREIGN KEY (donation_id) REFERENCES CRUK.donation(id)
+	CONSTRAINT user_donation_map_FK_1 FOREIGN KEY (donation_id) REFERENCES CRUK.donations(id)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT IGNORE INTO CRUK.users (id, email) 
+VALUES 
+(1,'testuser01@gmail.com'), 
+(2,'testuser02@gmail.com'),
+(3,'testuser03@gmail.com');
+
+INSERT IGNORE INTO CRUK.donations (id, donation_amount) 
+VALUES 
+(1,2000), 
+(2,2000), 
+(3,1000),
+(4,1000);
+
+INSERT IGNORE INTO CRUK.user_donation_map (donation_id,user_id) 
+VALUES 
+(1,1), 
+(2,1), 
+(3,1),
+(4,2);
