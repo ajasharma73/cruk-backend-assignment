3053c878250aac41f375aa332cd839b716c849a0
ajasharma73 <122118561+ajasharma73@users.noreply.github.com>
2023-01-10 22:21:03 +0000

    refactored code for lambda function

--- resources/database/DBService.ts
+++ resources/database/DBService.ts
@@ -1,7 +1,6 @@
 import knex, { Knex } from "knex";
 import { DATABASE_INIT_FAILED } from "../constants/errors";
 import { DATABASE_NAME, DATABASE_SECRET_NAME } from "../env";
-import LogService from "../services/LogService";
 import { MySqlConnectionConfig } from "../types/database.types";
 import { getSecretValue, GetSecretValueRet } from "../utils/secretValue";
 class DBService {
@@ -15,25 +14,30 @@ class DBService {
     }
 
     static async initialiseDb(){
-        const { password, username, host } = (await getSecretValue(DATABASE_SECRET_NAME)) as GetSecretValueRet;
-        const connectionConfig: MySqlConnectionConfig = {
-            user: username,
-            host,
-            password,
-            database: DATABASE_NAME
-          };
+        try{
+            const { password, username, host } = (await getSecretValue(DATABASE_SECRET_NAME)) as GetSecretValueRet;
+            console.log(password, username, host);
+            const connectionConfig: MySqlConnectionConfig = {
+                user: username,
+                host,
+                password,
+                database: DATABASE_NAME
+            };
 
-        this._db = knex({
-            client: 'mysql2',
-            connection: connectionConfig,
-            pool: { min: 0, max: 10 }
-          });
+            this._db = knex({
+                client: 'mysql2',
+                connection: connectionConfig,
+                pool: { min: 0, max: 10 }
+            });
+        }catch(err){
+            console.error(JSON.stringify(err));
+        }
 
         try{
             await this.checkConnection();
             return true;
         }catch(err){
-            LogService.logError(DATABASE_INIT_FAILED,err as Error);
+            console.error(DATABASE_INIT_FAILED,err as Error);
         }
         return false;
     }
--- resources/models/donations.ts
+++ resources/models/donations.ts
@@ -8,7 +8,7 @@ type GetNumberOfDonationsResult = {
 export const getNumberOfDonations = async (email: string):Promise<GetNumberOfDonationsResult | undefined> => {
     const db = DBService.getConnection();
     return await db(`${TABLES.DONATIONS} as d`)
-        .join(`${TABLES.USER_DONATION_MAP} as u_d_map`, "d.id", "d.donation_id")
+        .join(`${TABLES.USER_DONATION_MAP} as u_d_map`, "d.id", "u_d_map.donation_id")
         .join(`${TABLES.USERS} as u`, "u.id", "u_d_map.user_id")
         .count("d.id as count")
         .where("u.email", email)
